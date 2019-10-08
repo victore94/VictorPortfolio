5cd4d2dbe7391cef621a3b4f60dd25ae8c8e5d79
Victor Lopez <victorel94@gmail.com>
2019-10-08 02:12:07 -0700

    Updates

---
+++ precache-manifest.e08ac153d732535700101e8e3e53e1ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925c442a8d7b0c625c0a0be74df059ed",
+    "url": "/VictorPortfolio/index.html"
+  },
+  {
+    "revision": "a77594662e1a08e0ea1e",
+    "url": "/VictorPortfolio/static/css/main.70e11740.chunk.css"
+  },
+  {
+    "revision": "f838b65f1543c7d0140b",
+    "url": "/VictorPortfolio/static/js/2.cf4d06aa.chunk.js"
+  },
+  {
+    "revision": "a77594662e1a08e0ea1e",
+    "url": "/VictorPortfolio/static/js/main.c9552861.chunk.js"
+  },
+  {
+    "revision": "84643610274480dddeb8",
+    "url": "/VictorPortfolio/static/js/runtime-main.48f203dd.js"
+  },
+  {
+    "revision": "3442021cce034cf7f7ec6b14031bc802",
+    "url": "/VictorPortfolio/static/media/IMG_0086.3442021c.PNG"
+  }
+]);
