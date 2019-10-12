0c7e2c286cc0d5817bb3567251460d0972c30970
Victor Lopez <victorel94@gmail.com>
2019-10-12 15:55:47 -0700

    done for now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
-// import Resume from './components/Resume'
+
 import { BrowserRouter, Route } from 'react-router-dom'
 
 
@@ -13,25 +13,16 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
         <div class='container'>
           <Navbar />
-          <div class='jumbotron'>
-            <div class='row'>
-              <Route exact path='/VictorPortfolio' component={Home} />
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/contact' component={Contact} />
-              {/* <Resume /> */}
-            </div>
-
-          </div>
+          <Route exact path='/VictorPortfolio' component={Home} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
         </div>
-
-
+        <footer></footer>
       </div>
     </BrowserRouter>
-
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,21 @@ const Contact = () => {
     //     props.history.push('/about')
     // }, 2000);
     return (
-        <div className='container'>
-            <h4 className='center'>Contact</h4>
-            <p>this is contact</p>
+        // <div className='container'>
+        //     <h4 className='center'>Contact</h4>
+        //     <p>this is contact</p>
+        // </div>
+        <div class='container'>
+
+
+            <a href="http://facebook.com" target="_blank"><i class="fab fa-facebook fa-5x" /></a>
+            <a href="http://twitter.com" target="_blank"><i class="fab fa-twitter fa-5x" /></a>
+
+            <a href="http://instagram.com" target="_blank"><i class="fab fa-instagram fa-5x" /></a>
+            <a href="https://github.com/victore94" target="_blank">
+                <i class="fab fa-github fa-5x" /></a>
         </div>
+
     )
 }
 export default Contact
